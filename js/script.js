59dda0ca7e7145e1dc5012b96c704ed7c9eceb0f
Evgenik <109359580+Jeko26@users.noreply.github.com>
2022-08-21 23:58:08 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+let burger = document.querySelector('.burger');
+let links = document.querySelector('.header__group--burger');
+burger.addEventListener('click', function () {
+	links.classList.toggle('burger--open');
+	burger.classList.toggle('burger--cross');
+});
+
 var swiper = new Swiper(".swiper__container", {
     cssMode: true,
     loop: true,
